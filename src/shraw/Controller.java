c64d004a4f79b403e0971e77feec874f035ae8c5
Vseslav Sekorin <VsSekorin@gmail.com>
2017-11-17 01:00:13 +0300

    #15 move action

--- src/shraw/Controller.java
+++ src/shraw/Controller.java
@@ -2,6 +2,7 @@ package shraw;
 
 import javafx.application.Platform;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import shraw.model.Rectangle;
@@ -11,30 +12,42 @@ import shraw.model.Shapes;
 public final class Controller {
     public Pane paint;
     public ChoiceBox choice;
+    public ToggleButton toggle;
 
     private Shapes shapes = new Shapes();
     private Shape shape = null;
 
     public void mPressed(final MouseEvent event) {
+        if (this.toggle.isSelected()) {
+            this.shape = this.shapes.find(event);
+        } else {
+            this.shape = this.chooseShape(event);
+        }
+    }
+
+    private Shape chooseShape(final MouseEvent event) {
         switch ((String) choice.getValue()) {
             case "Rectangle":
-                this.shape = new Rectangle(event.getX(), event.getY());
-                break;
+                return new Rectangle(event.getX(), event.getY());
             default:
                 throw new IllegalStateException();
         }
     }
 
-    public void mReleased(final MouseEvent event) {
-        this.shapes.add(this.shape);
-        this.shape = null;
-    }
-
     public void mDragged(final MouseEvent event) {
-        this.shape.update(event);
+        if (this.toggle.isSelected()) {
+            this.shape.move(event);
+        } else {
+            this.shape.update(event);
+        }
         this.paint.getChildren().clear();
         this.shapes.get().forEach(this.paint.getChildren()::add);
         this.paint.getChildren().add(this.shape.asJavaFXShape());
+    }
+
+    public void mReleased() {
+        this.shapes.add(this.shape);
+        this.shape = null;
     }
 
     public void close() {
