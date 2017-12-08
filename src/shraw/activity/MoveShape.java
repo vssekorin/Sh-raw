dffdb4e41dea290c4ae2c508bbedc260b0295563
Vseslav Sekorin <VsSekorin@gmail.com>
2017-12-08 22:42:37 +0300

    move

---
+++ src/shraw/activity/MoveShape.java
@@ -0,0 +1,22 @@
+package shraw.activity;
+
+/**
+ * @author VsSekorin
+ */
+public final class MoveShape implements Activity {
+    @Override
+    public void undo() {
+        //TODO
+    }
+
+    @Override
+    public void redo() {
+        //TODO
+    }
+
+    @Override
+    public String name() {
+        //TODO
+        return null;
+    }
+}
